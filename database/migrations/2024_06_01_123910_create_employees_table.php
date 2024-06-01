b618dd7e9155c02146cf9c81fada500d0644a5ed
BRBRS <jnred0916@gmail.com>
2024-06-01 20:48:18 +0800

    created column name

---
+++ database/migrations/2024_06_01_123910_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('type');
+            $table->string('employment_status');
+            $table->string('birthdate');
+            $table->string('skill');
+            $table->string('skill_category');
+            $table->string('address');
+            $table->string('contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
--- database/migrations/2024_06_01_124011_create_attendances_table.php
+++ database/migrations/2024_06_01_124011_create_attendances_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attendances', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('password');
+            $table->string('assigned_member_id');
+            $table->string('date');
             $table->string('status');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attendances');
     }
 };
