052590fa7de18f95041da3d3ec40d08b7f03d41d
BRBRS <jnred0916@gmail.com>
2024-06-09 22:21:21 +0800

    updated web

--- routes/web.php
+++ routes/web.php
@@ -15,56 +15,49 @@ use App\Livewire\Component\ProjectManager\ProjectManagement\ProjectSummary;
 use App\Livewire\Dashboard\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-// Dashboard routes
-Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'dashboard'], function () {
-    Route::get('/', Dashboard::class)->name('dashboard.index');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// login
-Route::get('/login', \App\Livewire\Component\Login::class)->name('login.index');
+//AUTHENTICATED USER
+Route::group(['middleware' => ['auth']], function () {
 
-// admin
-// account management
-
-Route::group(['prefix' => 'account-management', 'middleware' => ['role:admin']], function() {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard.index');
+    // , 'middleware' => ['role:admin']]
+    Route::group(['prefix' => 'account-management'], function() {
    
-    Route::get('/', AccountManagement::class)->name('account-management.index');
-    Route::get('/add', AddAccount::class)->name('account.create');
-    Route::get('/profile/{member}', AccountProfile::class)->name('profile.index');
-    Route::get('/profile/edit/{member}', EditProfile::class)->name('profile.edit');
+        Route::get('/', AccountManagement::class)->name('account-management.index');
+        Route::get('/add', AddAccount::class)->name('account.create');
+        Route::get('/profile/{member}', AccountProfile::class)->name('profile.index');
+        Route::get('/profile/edit/{member}', EditProfile::class)->name('profile.edit');
+        
+    });
     
+    
+    // Project Manager
+    Route::get('/manpower', ManpowerList::class)->name('manpower.index');
+    Route::get('/project-management', Project::class)->name('project-management.index');
+    Route::get('/project-management/add', AddProject::class)->name('project.create');
+    Route::get('/project/{project}', ProjectSummary::class)->name('project-summary.index');
+    Route::get('/project/edit/{project}', EditProject::class)->name('project.edit');
+    
+    
+    // sample only
+    Route::get('/logbook', Logbook::class)->name('logbook.index');
+
+});
+
+//LOGIN
+Route::group(['middleware' => ['guest']], function() {
+    Route::get('/login', \App\Livewire\Component\Login::class)->name('login.index');
 });
 
 
-// Project Manager
-Route::get('/manpower', ManpowerList::class)->name('manpower.index');
-Route::get('/project-management', Project::class)->name('project-management.index');
-Route::get('/project-management/add', AddProject::class)->name('project.create');
-Route::get('/project/id', ProjectSummary::class)->name('project-summary.index');
-Route::get('/project/edit', EditProject::class)->name('project.edit');
-
-
-
-// sample only
-Route::get('/logbook', Logbook::class)->name('logbook.index');
 
 // Authentication routes
-Route::group(['prefix' => 'auth'], function () {
-    Route::get('login', Login::class)->name('login');
-    Route::get('register', Register::class)->name('register');
-});
+// Route::group(['prefix' => 'auth'], function () {
+//     Route::get('login', Login::class)->name('login');
+//     Route::get('register', Register::class)->name('register');
+// });
