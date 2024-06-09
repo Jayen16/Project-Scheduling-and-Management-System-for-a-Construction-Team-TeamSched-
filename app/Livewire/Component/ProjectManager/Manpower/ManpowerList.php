af2f52afdef57c97aed98d4848fff86f3fb483e7
BRBRS <jnred0916@gmail.com>
2024-06-09 22:20:05 +0800

    display manpowerlist

--- app/Livewire/Component/ProjectManager/Manpower/ManpowerList.php
+++ app/Livewire/Component/ProjectManager/Manpower/ManpowerList.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire\Component\ProjectManager\Manpower;
 
+use App\Models\Employee;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ManpowerList extends Component
 {
+
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    
+    public $manpowers;
+    public $filter = 'all';
+    public $search = '';
+
     public function render()
     {
-        return view('livewire.component.project-manager.manpower.manpower-list');
+        $this->manpowers = Employee::where('type','manpower')->get();
+
+        $query = Employee::where('type', 'manpower');
+    
+        if ($this->filter !== 'all') {
+            $query->where('skill', $this->filter);
+        }
+    
+        if (!empty($this->search)) {
+            $query->where(function ($subQuery) {
+                $subQuery->where('firstName', 'like', '%' . $this->search . '%')
+                            ->orWhere('lastName', 'like', '%' . $this->search . '%');
+            });
+        }
+    
+        $paginate = $query->paginate(10);
+    
+        return view('livewire.component.project-manager.manpower.manpower-list', ['paginate' => $paginate]);
+        
     }
 }
