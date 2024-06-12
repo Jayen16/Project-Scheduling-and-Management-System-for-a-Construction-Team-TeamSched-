5367cc60922cc1876989d04bfd2e65a07aa7af59
BRBRS <jnred0916@gmail.com>
2024-06-12 22:59:13 +0800

    updated edit project but not done yet with editing manpower

--- resources/views/livewire/component/project-manager/project-management/edit-project.blade.php
+++ resources/views/livewire/component/project-manager/project-management/edit-project.blade.php
@@ -7,7 +7,7 @@
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route('project-management.index') }}">Project
                                 Management</a></li>
-                        <li class="breadcrumb-item"><a href="{{ route('project-summary.index') }}">Project
+                        <li class="breadcrumb-item"><a href="{{ route('project-summary.index',['project'=>$project->id]) }}">Project
                                 Summary</a></li>
                         <li class="breadcrumb-item active">Edit</li>
                     </ol>
@@ -33,7 +33,7 @@
         </div>
 
 
-        <form wire:submit="">
+        <form>
             <div class="card card-primary">
                 <div class="card-header">
                     <h3 class="card-title">Project Details</h3>
@@ -43,10 +43,11 @@
                         <div class="col-4">
                             <div class="form-group">
                                 <label>Title <span class="text-danger font-weight-bolder text-md">*</span></label>
-                                <input wire:model='' type="text" class="form-control" placeholder="Title here..."
+                                <input wire:model='project_name' type="text" class="form-control" placeholder="Title here..."
                                     required>
                             </div>
                         </div>
+
                         {{-- daterange picker --}}
                         <div class="col-3">
                             <div class="form-group">
@@ -58,7 +59,7 @@
                                             <i class="far fa-calendar-alt"></i>
                                         </span>
                                     </div>
-                                    <input type="text" class="form-control float-right" id="reservation"
+                                    <input wire:model='project_date_range' type="text" class="form-control float-right" id="reservation"
                                         name="dates">
                                 </div>
                             </div>
@@ -68,7 +69,7 @@
                         <div class="col-12">
                             <div class="form-group">
                                 <label>Description <span class="text-danger font-weight-bolder text-md">*</span></label>
-                                <textarea style="resize: none;" class="form-control" rows="3" placeholder="Enter project description here..."></textarea>
+                                <textarea wire:model='project_description' style="resize: none;" class="form-control" rows="3" placeholder="Enter project description here..."></textarea>
                             </div>
                         </div>
                     </div>
@@ -79,7 +80,8 @@
             </div>
 
             <div>
-                @foreach ($scopes as $index => $scope)
+                @foreach ($project_scope as $index => $week)
+
                     <div class="card card-secondary mb-3">
                         <div class="card-header">
                             <h3 class="card-title">Scope of Work</h3>
@@ -95,7 +97,8 @@
                                     <div class="form-group">
                                         <label>Title <span
                                                 class="text-danger font-weight-bolder text-md">*</span></label>
-                                        <input wire:model='' type="text" class="form-control text-uppercase"
+                                          
+                                        <input wire:model='week_title.{{ $index }}' type="text" class="form-control text-uppercase"
                                             placeholder="Enter Title..." required>
                                     </div>
                                 </div>
@@ -107,11 +110,11 @@
                                             <div class="mr-2">
                                                 <label>Task <span
                                                         class="text-danger font-weight-bolder text-md">*</span></label>
-                                                <input wire:model='task' type="text" class="form-control"
+                                                <input wire:model='tasks.{{ $index }}' type="text" class="form-control"
                                                     style="width:250px;" placeholder="Enter Task here..." required>
                                             </div>
                                             <div>
-                                                <button wire:click='addTask({{ $index }})' type="button"
+                                                <button wire:click="addTask('{{ $index }}')" type="button"
                                                     class="btn btn-success">
                                                     <i class="nav-icon fas fa-plus mr-2"></i>Add
                                                 </button>
@@ -119,23 +122,28 @@
                                         </div>
                                     </div>
                                 </div>
+
+     
                                 <div class="col-4">
                                     <div class="form-group">
+                                        
                                         <div class="d-flex align-items-end">
                                             <div class="mr-2">
                                                 <label>Assign Manpower <span
                                                         class="text-danger font-weight-bolder text-md">*</span></label>
-                                                <select style="width: 250px;" wire:model="manpower" class="form-control"
+                                                <select style="width: 250px;" wire:model="manpowers.{{ $index }}" class="form-control"
                                                     required>
-                                                    <option value="">Select Role</option>
-                                                    <option value="Manpower 1">Manpower 1</option>
-                                                    <option value="Manpower 2">Manpower 2</option>
-                                                    <option value="Manpower 3">Manpower 3</option>
-                                                    <option value="Manpower 4">Manpower 4</option>
+                                                     <option value="">Select Role</option>
+                                 
+                                                    @foreach ($listedManpower as $manpower)
+                                                        @if (!in_array($manpower['id'],$week['manpowers']))
+                                                            <option value="{{ $manpower['id']}}">{{ $manpower['firstName'].''.$manpower['middleName'].''.$manpower['lastName']}}</option>
+                                                        @endif
+                                                    @endforeach
                                                 </select>
                                             </div>
                                             <div>
-                                                <button wire:click='addManpower({{ $index }})' type="button"
+                                                <button wire:click="addManpower('{{ $index }}')" type="button"
                                                     class="btn btn-success">
                                                     <i class="nav-icon fas fa-plus mr-2"></i>Add
                                                 </button>
@@ -148,7 +156,9 @@
                                 <div class="col-4">
                                     <div class="form-group">
                                         <label>Tasks:</label>
-                                        @foreach ($scope['tasks'] as $taskIndex => $task)
+                                        @php
+                                        @endphp
+                                        @foreach ($week['tasks'] as $taskIndex => $task)
                                             <div class="d-flex align-items-end mt-2">
                                                 <div class="mr-2">
                                                     <div style="background-color: rgb(248, 248, 248); width:250px;"
@@ -157,7 +167,7 @@
                                                     </div>
                                                 </div>
                                                 <button
-                                                    wire:click='removeTask({{ $index }}, {{ $taskIndex }})'
+                                                    wire:click="removeTask('{{ $index }}','{{ $taskIndex }}', '{{ $task }}')"
                                                     type="button" class="btn btn-danger btn-sm">
                                                     <i class="nav-icon fas fa-minus mr-2"></i>Remove
                                                 </button>
@@ -168,16 +178,20 @@
                                 <div class="col-4">
                                     <div class="form-group">
                                         <label>Manpower:</label>
-                                        @foreach ($scope['manpowers'] as $manpowerIndex => $manpower)
+                                        @foreach ($week['manpowers'] as $manpowerIndex => $manpowerID)
                                             <div class="d-flex align-items-end mt-2">
                                                 <div class="mr-2">
                                                     <div style="background-color: rgb(248, 248, 248); width:250px;"
                                                         class="p-1 pl-2 border rounded text-uppercase">
-                                                        {{ $manpower }}
+
+                                                        @php
+                                                            $display_manpower = App\Models\Employee::where('id',$manpowerID)->first();
+                                                        @endphp
+                                                        {{ $display_manpower->firstName.''.$display_manpower->middleName.''.$display_manpower->lastName }}
                                                     </div>
                                                 </div>
                                                 <button
-                                                    wire:click='removeManpower({{ $index }}, {{ $manpowerIndex }})'
+                                                    wire:click="removeManpower('{{ $index }}','{{ $manpowerIndex }}', '{{ $manpowerID }}')"
                                                     type="button" class="btn btn-danger btn-sm">
                                                     <i class="nav-icon fas fa-minus mr-2"></i>Remove
                                                 </button>
@@ -199,7 +213,7 @@
             <div class="d-flex justify-content-end mt-4">
                 <button wire:click='redirectToProjectSummary()' type="button" class="btn btn-default mr-2">
                     Cancel</button>
-                <button type="submit" class="btn btn-success float-right">Save</button>
+                <button type="button" wire:click='update()' class="btn btn-success float-right">Save</button>
             </div>
         </form>
     </div>
