6126fa28c2dbd3bcd76e6a7abab12efce307f567
BRBRS <jnred0916@gmail.com>
2024-06-20 10:45:01 +0800

    set active in factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
 
 
         \App\Models\User::factory(5)->hasEmployee(['type'=>'manpower'])->asManpower()->create();
-        \App\Models\User::factory(1)->hasEmployee(['type'=>'admin'])->asAdmin()->create();
+        \App\Models\User::factory(1)->hasEmployee(['type'=>'admin','status'=>'Active'])->asAdmin()->create();
         \App\Models\User::factory(1)->hasEmployee(['type'=>'supervisor'])->asSuperVisor()->create();
         \App\Models\User::factory(1)->hasEmployee(['type'=>'manager'])->asManager()->create();
 
