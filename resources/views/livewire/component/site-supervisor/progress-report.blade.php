8c18018b3a33c2cee9d4e1ec0043da3c676c2811
Jayen16 <107201690+Jayen16@users.noreply.github.com>
2024-06-20 14:52:58 +0800

    Update progress-report.blade.php

--- resources/views/livewire/component/site-supervisor/progress-report.blade.php
+++ resources/views/livewire/component/site-supervisor/progress-report.blade.php
@@ -66,7 +66,7 @@
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="markAsDoneLabel={{ $task->id }}">Delete Project</h5>
+                                                    <h5 class="modal-title" id="markAsDoneLabel={{ $task->id }}">Mask as Done </h5>
                                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                         <span aria-hidden="true">&times;</span>
                                                     </button>
